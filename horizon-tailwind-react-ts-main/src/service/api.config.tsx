fd3021616baab5f9d02853ccbee1d8d369363475
quyet <vuminhquyet18@gmail.com>
2025-06-06 20:35:43 +0700

    a

--- horizon-tailwind-react-ts-main/src/service/api.config.tsx
+++ horizon-tailwind-react-ts-main/src/service/api.config.tsx
@@ -1,5 +1,5 @@
 //export const API_BASE_URL = 'http://14.225.198.3:8080';
-export const API_BASE_URL = '';
+export const API_BASE_URL = 'http://localhost:8080';
 export const API_ENDPOINTS = {
     ADMIN: {
         LOGIN: '/api/v1/admin/login',
